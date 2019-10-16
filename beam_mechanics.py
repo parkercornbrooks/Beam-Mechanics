58733a8299c14a95c6738820711c8c75831aecf5
Parker Cornbrooks <pcorn123@gmail.com>
2019-10-16 00:22:59 -0400

    added documentation to beam class

--- beam_mechanics.py
+++ beam_mechanics.py
@@ -1,7 +1,47 @@
 import numpy as np
 
 class Beam:
-    def __init__(self, length, units = "m"):
+    """
+    A class used to represent a simple beam under loading
+
+    ...
+
+    Atrributes
+    ----------
+    length : float
+        the length of the beam
+    units : str
+        the units of the length (default "meter")
+    loads : list 
+        list of loads applied to beam
+    supports : list
+        list of beam supports
+    
+    Methods
+    -------
+    add_pointload(pos, force)
+        adds a point load to the beam of given force at given position
+    add_uniform_distload(start, end, force)
+        adds a uniform distributed load from a start to end position of given force
+    add_distLoad(start, end, startForce, endForce)
+        add a distributed load with a given start/end position/force (units in force/length)
+    add_moment(pos, moment)
+        add a point moment at the given position
+    add_support(style, pos)
+        add a support of given style at the given position
+    solve(position = None, N = 100)
+        solve for the shear and moment values at the given positions
+    """
+
+    def __init__(self, length, units = "meter"):
+        """
+        Parameters
+        ----------
+        length : float
+            the length of the beam
+        units : str, optional
+            the units of length (default is "meter")
+        """
         self.length = length
         self.units = units
         self.loads = []
@@ -14,27 +54,77 @@ class Beam:
         return s
     
     def add_pointLoad(self, pos, force):
-        if pos < 0 or pos > self.length:
-            print(f"Force must be within the span of the beam (0-{self.length}m)")
-        else:
-            self.loads.append(PointLoad(pos, force))
+        """Creates a point load and adds it to the loads list
+
+        Parameters
+        ----------
+        pos : float
+            the position on the beam where the point load is applied
+        force : float
+            the force of the point load
+        """
+        _check_location(self.length, pos)
+        self.loads.append(PointLoad(pos, force))
 
     def add_uniform_distLoad(self, start, end, force):
+        """Creates a uniform distributed load and adds it to the loads list
+
+        Parameters
+        ----------
+        start : float
+            the position on the beam where the distload starts
+        end : float
+            the position on the beam where the distload ends
+        force : float
+            the force, in force per unit length
+        """
+        _check_location(self.length, start, end)
         self.loads.append(DistLoad(start, end, force, force))
     
     def add_distLoad(self, start, end, startForce, endForce):
+        """Creates a uniform distributed load and adds it to the loads list
+
+        Parameters
+        ----------
+        start : float
+            the position on the beam where the distload starts
+        end : float
+            the position on the beam where the distload ends
+        startForce : float
+            the force, in force per unit length, at the start position
+        endForce : float
+            the force, in force per unit length, at the end position
+        """
+        _check_location(self.length, start, end)
         self.loads.append(DistLoad(start, end, startForce, endForce))
     
     def add_moment(self, pos, moment):
+        """Creates a point moment and adds it to the loads list
+
+        Parameters
+        ----------
+        pos : float
+            the position on the beam where the moment is applied
+        moment : float
+            the magnitude of the moment
+        """
+        _check_location(self.length, pos)
         self.loads.append(PointMoment(pos, moment))
 
     def add_support(self, style, pos):
+        """Creates a support and adds it to the supports list
+
+        Parameters
+        ----------
+        style : str
+            the style of support (pin, roller, fixed)
+        pos : float
+            the position on the beam where the moment is applied
+        """
+        _check_location(self.length, pos)
         check = True
         if len(self.supports) == 2:
             print("Adding more than 2 supports is not allowed")
-            check = False
-        elif pos < 0 or pos > self.length:
-            print(f"Support must be within the span of the beam (0-{self.length}m)")
             check = False
         elif len(self.supports) == 1:
             if self.supports[0].unknowns == 3:
@@ -62,11 +152,29 @@ class Beam:
                     print(f"Fixed support added at {pos}.")
 
     def solve(self, positions = None, N = 100):
-        
+        """Solve for the shear and moment values at the given positions
+
+        Parameters
+        ----------
+        positions : int/float, list of int/float, or numpy array of int/float, optional
+            the position or positions to solve for shear and moment
+            default is a linear spaced array in [0, beam length] with N points
+        N : int, optional
+            the number of positions to solve within the linear spaced array (default 100)
+            if positions are given, this value is not used
+
+        Returns
+        -------
+        dictionary
+            keys are "positions", "moment", "shear"
+            values are numpy arrays
+        """
+
         if positions == None:
             positions = np.linspace(0, self.length, N)
-        else:
+        else:  # convert list to numpy array of floats
             positions = np.array(positions, dtype=float)
+        
         # ToDo: solve for support reactions
         
         shear = sum([load.calc_shear(positions) for load in self.loads])
@@ -74,10 +182,6 @@ class Beam:
         return {"positions" : positions,
                 "moment" : moment,
                 "shear" : shear}
-
-def is_indeterminate(beam):
-    unknowns = sum([support.unknowns for support in beam.supports])
-    return True if unknowns > 3 else False
 
 class PointLoad:
     def __init__(self, pos, force):
@@ -98,11 +202,17 @@ class PointLoad:
 
 class DistLoad:
     def __init__(self, start, end, startForce, endForce):
-        self.start = start
-        self.end = end
+        self.start, self.end = self._check_loc(start, end)
         self.startForce = startForce
         self.endForce = endForce
         (self.force, self.pos) = self.resultant()
+    
+    def _check_loc(self, start, end):
+        if start == end:
+            raise ValueError("Start and end positions must be different")
+        if start > end:
+            start, end = end, start
+        return start, end
 
     def resultant(self):
         return resultant(self.start, self.end, self.startForce, self.endForce)
@@ -182,8 +292,25 @@ class PinSupport(Support):
 
 
 def resultant(x1, x2, y1, y2):
-    ''' returns a tuple with resultant force and location
-    holds true for uniform loads, triangle, and trapezoid'''
+    ''' Calculates resultant force of a distributed load
+    Can be used for uniform loads (y1 = y2), triangle (y1 or y2 = 0), or trapezoidal
+    
+    Parameters
+    ----------
+    x1 : float
+        the start position of the load
+    x2 : float
+        the end position of the load
+    y1 : float
+        the start force of the load
+    y2 : float
+        the end force of the load
+
+    Returns
+    -------
+    tuple 
+        a tuple of resultant force and location (from position 0)
+    '''
 
     ymin = min(y1, y2)
     b = (x2 - x1)
@@ -202,23 +329,19 @@ def resultant(x1, x2, y1, y2):
     dr = -1. if R == 0 else ((F1 * d1) + (F2 * d2)) / R
     return (R, dr)
 
-def main():
-    #resultant_test()
-    solve_test()
+def _check_location(length, *points):
+    """ Checks all points to see if they fall within [0,length]
     
-    
-def resultant_test():
-    L = DistLoad(0, 5, 5, 0)
-    x = np.linspace(0, 8, 20)
-    print(x)
-    print(L.calc_shear(x))
-    print(L.calc_moment(x))
+    Raises ValueError if any point falls out of bounds
+    """
+    for point in points:
+        if point < 0 or point > length:
+            raise ValueError(f"Point {point} is not located on the beam of length 0-{length}")
 
-def solve_test():
-    bm = Beam(10)
-    bm.add_pointLoad(5,5)
-    d = bm.solve()
-    print(d["moment"])
+
+
+def main():
+    pass
 
 if __name__ == "__main__":
     main()
